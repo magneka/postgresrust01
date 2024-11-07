0a7aba622740bd051fb768d49c57c3fe2b1e9053
Magne Alvheim <magne@alvheim.com>
2024-11-07 09:59:02 +0100

    Fix

--- source/src/datalayer/repositories/repository_tools.rs
+++ source/src/datalayer/repositories/repository_tools.rs
@@ -1,25 +1,27 @@
 #![allow(unused_imports)]
 use sqlx::postgres::{self, PgPoolOptions, PgRow};
-use sqlx::{query_as, Encode, Error, FromRow, Pool, Postgres, Row};
 use sqlx::types::chrono::{DateTime, Utc};
-
+use sqlx::{query_as, Encode, Error, FromRow, Pool, Postgres, Row};
 
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
 pub enum ParameterType {
-    StringType (String),
+    StringType(String),
     Integer16(i16),
     Integer32(i32),
 }
 
-
-pub fn set_parameter<T>(select_query: sqlx::query::QueryAs<'_, Postgres, T, postgres::PgArguments>, search_for: ParameterType) -> sqlx::query::QueryAs<'_, Postgres, T, postgres::PgArguments> {
+pub fn set_parameter<T>(
+    p_select_query: sqlx::query::QueryAs<'_, Postgres, T, postgres::PgArguments>,
+    search_for: ParameterType,
+) -> sqlx::query::QueryAs<'_, Postgres, T, postgres::PgArguments> {
     
-let _select_query: sqlx::query::QueryAs<'_, Postgres, _, postgres::PgArguments> = match search_for {
-    ParameterType::StringType ( s ) => select_query.bind(s.clone()),
-    ParameterType::Integer16(s) => select_query.bind(s),
-    ParameterType::Integer32(s) => select_query.bind(s),
-};
+    let r_select_query: sqlx::query::QueryAs<'_, Postgres, _, postgres::PgArguments> =
+        match search_for {
+            ParameterType::StringType(s) => p_select_query.bind(s.clone()),
+            ParameterType::Integer16(s) => p_select_query.bind(s),
+            ParameterType::Integer32(s) => p_select_query.bind(s),
+        };
 
-_select_query
+    r_select_query
 }
